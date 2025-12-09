f6061f82d0cf85545b189395ca74a65cf7faff80
OppositeOdd <116982635+OppositeOdd@users.noreply.github.com>
2025-12-08 17:28:43 -0700

    Fix dependency parsing in build script
    - Changed cut -c 12- to cut -c 13- to skip the space after '# requires:'
    - This removes the extra leading space in dependency names
    - Fixes auto-selection of funUtil dependency in Stash UI

--- build_site.sh
+++ build_site.sh
@@ -43,7 +43,7 @@ buildPlugin()
     ymlVersion=$(grep "^version:" "$f" | head -n 1 | cut -d' ' -f2- | sed -e 's/^"\(.*\)"$/\1/')
     version="$ymlVersion-$version"
     # set IFS
-    IFS=$'\n' dep=$(grep "^# requires:" "$f" | cut -c 12- | sed -e 's/\r//')
+    IFS=$'\n' dep=$(grep "^# requires:" "$f" | cut -c 13- | sed -e 's/\r//')
 
     # write to spec index
     echo "- id: $plugin_id
